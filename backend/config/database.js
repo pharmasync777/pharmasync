34efe633129da84491a2d6b10b42aa07237c951c
Aurell Rajwa Afianto <133080612+rajhigh@users.noreply.github.com>
2026-05-30 00:54:49 +0700

    PharmaSync Fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2');
-require('dotenv').config({ path: require('path').join(__dirname, '.env') });
+require('dotenv').config();
 
 const pool = mysql.createPool({
     host:     process.env.DB_HOST,
